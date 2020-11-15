acaf7a3ecf806a7bfc2e18ce1bba43c2842bec65
Enis Mulić <enis.s.mulic@gmail.com>
2020-11-15 09:16:20 +0100

    Create direct access point to the database

--- database/cog.py
+++ database/cog.py
@@ -1,24 +1,24 @@
 from pymongo import errors
-from .context import client
+from .context import db
 
 def get_all():
-    return [doc for doc in client.msg_z_config.configs.find()]
+    return [doc for doc in db.configs.find()]
 
 def get(name: str):
-    return client.msg_z_config.configs.find_one({"name": name})
+    return db.configs.find_one({"name": name})
 
 def create(config):
-    result = client.msg_z_config.configs.insert_one(config)
+    result = db.configs.insert_one(config)
     if result.acknowledged == True:
         return config
     return None
 
 def update(name, config):
-    result = client.msg_z_config.configs.update_one({"name": name}, {"$set": config})
+    result = db.configs.update_one({"name": name}, {"$set": config})
     if result.modified_count == 1:
         return config
     return None
 
 def delete(name):
-    result = client.msg_z_config.configs.delete_one({"name": name})
+    result = db.configs.delete_one({"name": name})
     return result.deleted_count != 0
--- database/context.py
+++ database/context.py
@@ -2,5 +2,5 @@ from pymongo import MongoClient
 import pymongo
 
 client = MongoClient("mongodb://localhost:27017/msg_z_config")
-
-client.msg_z_config.configs.create_index([('name', pymongo.ASCENDING)], unique=True)
+db = client.msg_z_config
+db.configs.create_index([('name', pymongo.ASCENDING)], unique=True)
