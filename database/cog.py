50e373cd62623ea6aecfc56575afba723cb7b595
Enis Mulić <enis.s.mulic@gmail.com>
2020-11-15 09:26:19 +0100

    Group methods into a class

--- database/cog.py
+++ database/cog.py
@@ -1,24 +1,28 @@
 from pymongo import errors
 from .context import db
 
-def get_all():
-    return [doc for doc in db.configs.find()]
+class CogRepository:
+    def __init__(self):
+        self.dbSet = db.configs
 
-def get(name: str):
-    return db.configs.find_one({"name": name})
+    def get_all(self):
+        return [doc for doc in self.dbSet.find()]
 
-def create(config):
-    result = db.configs.insert_one(config)
-    if result.acknowledged == True:
-        return config
-    return None
+    def get(self, name: str):
+        return self.dbSet.find_one({"name": name})
 
-def update(name, config):
-    result = db.configs.update_one({"name": name}, {"$set": config})
-    if result.modified_count == 1:
-        return config
-    return None
+    def create(self, config):
+        result = self.dbSet.insert_one(config)
+        if result.acknowledged == True:
+            return config
+        return None
 
-def delete(name):
-    result = db.configs.delete_one({"name": name})
-    return result.deleted_count != 0
+    def update(self, name, config):
+        result = self.dbSet.update_one({"name": name}, {"$set": config})
+        if result.modified_count == 1:
+            return config
+        return None
+
+    def delete(self, name):
+        result = self.dbSet.delete_one({"name": name})
+        return result.deleted_count != 0
